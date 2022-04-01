d181aa2e2467e04eb8f60612bf548a8f73f33209
AleixoLeonardo <aleixo1leonardo@gmail.com>
2022-03-31 21:02:18 -0300

    created user crud

--- shared/service/user.service.js
+++ shared/service/user.service.js
@@ -1,12 +1,51 @@
 const db = require('./db.service');
 const { USER_COLLECTION } = require('../constants/constants');
+const { ObjectId } = require('mongodb');
 
+USER_EXIST =  "Usuário já existe";
+
+
+const getByEmail = async (email) => {
+    return await db.getMainDB().then(conn => {
+        return conn.collection(USER_COLLECTION).findOne({ "email": { $eq: email } });
+    })
+};
 
 const getAll = async () => {
     return await db.getMainDB().then(conn => {
-        console.log(USER_COLLECTION)
         return conn.collection(USER_COLLECTION).find().toArray();
+    })
+};
+
+const getById = async (id) => {
+    return await db.getMainDB().then(conn => {
+        return conn.collection(USER_COLLECTION).findOne({ _id: ObjectId(id) }).then(user => {
+            return user;
+        })
     })
 }
 
-module.exports = { getAll }
+const create = async (user) => {
+    const userDB = await getByEmail(user.email);
+    if (userDB) {
+        throw USER_EXIST;
+    }
+    await db.getMainDB().then(conn => {
+        conn.collection(USER_COLLECTION).insertOne(user);
+    })
+};
+
+const update = async (user) => {
+    const userDB = await getByEmail(user.email);
+    if (userDB && user._id != userDB._id) {
+        throw USER_EXIST;
+    }
+    await db.getMainDB().then(conn => {
+        conn.collection(USER_COLLECTION).updateOne(
+            { _id: user._id },
+            { $set: user }
+        )
+    })
+};
+
+module.exports = { getAll, create, update, getById }
